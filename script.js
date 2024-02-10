dc41bf03d062816e93b9a98414363cacf0bd6975
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-02-10 09:15:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function mapLetters(word){
+	let obj = {};
+	for(let i = 0; i < word.length; i++){
+		let letter = word[i];
+		if(obj[letter] === undefine){
+			obj[letter] = [i];
+		}else{
+			obj[letter].push(i);
+		}
+	}
+	return obj;
+}
+console.log(mapLetters("dodo"));
